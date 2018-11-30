82d0476b89c2524989059e605970d7c027018b73
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-11-30 23:34:01 +0100

    Create main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
