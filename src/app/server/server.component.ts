65f2e8d599c72e4801dc923815302fd48f354334
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-01 21:46:57 +0100

    Add new paragraph to server component
    Uses string interpolation and method returning string

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   serverId: number = 100;
   status: string = 'online';
+
+  getStatus() {
+    return this.status;
+  }
  }
