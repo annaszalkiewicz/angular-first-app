2186556e6d048f356c9b8a6ecac2ea8cc6b5dd97
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-04 01:28:04 +0100

    Show paragraph only when button is clicked
    Add onButtonClick method to button that set showParagraph
    property to true. Use ngIf directive to show paragraph
    on button clicked

--- src/app/assignment-three/assignment-three.component.ts
+++ src/app/assignment-three/assignment-three.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-assignment-three',
   templateUrl: './assignment-three.component.html',
-  styleUrls: ['./assignment-three.component.css']
+  styleUrls: ['./assignment-three.component.css'],
 })
 export class AssignmentThreeComponent implements OnInit {
+
+  count = 0;
+  showParagraph = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onButtonClick() {
+    this.showParagraph = true;
   }
 
 }
