69c8f54321669385abca50c8f550124edc1e5d54
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-14 22:24:35 +0100

    Add databinding with Output decorator and emit()

--- src/app/cmp-databinding/cmp-databinding.component.ts
+++ src/app/cmp-databinding/cmp-databinding.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-cmp-databinding',
-  templateUrl: './cmp-databinding.component.html',
-  styleUrls: ['./cmp-databinding.component.css']
+  selector: "app-cmp-databinding",
+  templateUrl: "./cmp-databinding.component.html",
+  styleUrls: ["./cmp-databinding.component.css"]
 })
 export class CmpDatabindingComponent implements OnInit {
+  serverElements = [
+    { type: "server", name: "Testserver", content: "This is test server" }
+  ];
 
-  serverElements = [{type: 'server', name: 'Testserver', content: 'This is test server'}];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onServerCreated(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
   }
 
+  onBlueprintCreated(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
