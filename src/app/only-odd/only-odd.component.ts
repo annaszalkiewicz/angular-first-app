493a215ea4a440ae29023fd14943d5451da1f601
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-27 19:49:49 +0100

    Add properties number and onlyOdd

--- src/app/only-odd/only-odd.component.ts
+++ src/app/only-odd/only-odd.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnlyOddComponent implements OnInit {
 
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
+
   constructor() { }
 
   ngOnInit() {
