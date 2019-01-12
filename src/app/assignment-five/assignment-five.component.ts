4e1580f6ee320bfebeeb35426a3c3dc56cb492f6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-13 00:27:00 +0100

    Remove unused data binding

--- src/app/assignment-five/assignment-five.component.ts
+++ src/app/assignment-five/assignment-five.component.ts
@@ -1,28 +1,17 @@
+import { UsersService } from './../users.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-assignment-five',
   templateUrl: './assignment-five.component.html',
-  styleUrls: ['./assignment-five.component.css']
+  styleUrls: ['./assignment-five.component.css'],
+  providers: [UsersService]
 })
 export class AssignmentFiveComponent implements OnInit {
-
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
-
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
   }
 
 }
